b86ce8fd9db4d5330656f9a77b4cb308b18efe97
vishalraj096 <vishal20031singh@gmail.com>
2026-05-21 19:22:06 +0530

    fixing actor service bug

--- backend/src/main/java/com/example/backend/service/film/ActorService.java
+++ backend/src/main/java/com/example/backend/service/film/ActorService.java
@@ -13,12 +13,14 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ActorService {
 
     private final ActorRepository actorRepository;
